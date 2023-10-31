d087d27114dade95cbc4fe7d37a72873fddacfbd
XuBao <3080879732@qq.com>
2023-10-31 21:50:36 +0800

    写好了初版完全体代码，构造了用于识别的face_recognition_gui类，再类内实现了录入照片、KNN分类器的训练、识别人脸三个功能。

--- knn_webcam.py
+++ knn_webcam.py
@@ -183,47 +183,15 @@ def predict(img, knn_clf=None, model_path=None, distance_threshold=0.6):
         )
     ]
 
-def show_prediction_labels_on_image(img_path, predictions):
-    """
-    Shows the face recognition results visually.
-
-    :param img_path: path to image to be recognized
-    :param predictions: results of the predict function
-    :return:
-    """
-    pil_image = Image.open(img_path).convert("RGB")
-    draw = ImageDraw.Draw(pil_image)
-
-    for name, (top, right, bottom, left) in predictions:
-        # Draw a box around the face using the Pillow module
-        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-
-        # There's a bug in Pillow where it blows up with non-UTF-8 text
-        # when using the default bitmap font
-        name = name.encode("UTF-8")
-
-        # Draw a label with a name below the face
-        text_width, text_height = draw.textsize(name)
-        draw.rectangle(
-            ((left, bottom - text_height - 10), (right, bottom)),
-            fill=(0, 0, 255),
-            outline=(0, 0, 255),
-        )
-        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
-
-    # Remove the drawing library from memory as per the Pillow docs
-    del draw
-
-    # Display the resulting image
-    pil_image.show()
-
 
 if __name__ == "__main__":
     # STEP 1: Train the KNN classifier and save it to disk
     # Once the model is trained and saved, you can skip this step next time.
     print("Training KNN classifier...")
     classifier = train(
-        "knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2
+        "knn_examples/train",
+        model_save_path="knn_examples/trained_knn_model.clf",
+        n_neighbors=2,
     )
     print("Training complete!")
 
@@ -237,7 +205,9 @@ if __name__ == "__main__":
         rgb_frame = frame[:, :, ::-1]
 
         # Find all the faces and face enqcodings in the frame of video
-        predictions = predict(rgb_frame, model_path="trained_knn_model.clf")
+        predictions = predict(
+            rgb_frame, model_path="knn_examples/trained_knn_model.clf"
+        )
 
         # Loop through each face in this frame of video
         for name, (top, right, bottom, left) in predictions:
